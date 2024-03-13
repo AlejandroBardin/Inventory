267612027c642d16e62417ec62c54bc28d7e0f85
Martin Casares <138946027+martin-casares@users.noreply.github.com>
2024-03-13 00:00:49 -0300

    fix-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { RegisterPage } from './pages/registerPage/RegisterPage';
-import { LoginPage } from './pages/loginPage/LoginPage';
 import { Dashboard } from './pages/dashboard/Dashboard';
 
 function App() {
@@ -9,8 +7,7 @@ function App() {
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<h1>Home Page</h1>} />
-				<Route path="/login" element={<LoginPage />} />
-				<Route path="/register" element={<RegisterPage />} />
+		
 				<Route path="/dashboard" element={<Dashboard />} />
 			</Routes>
 		</BrowserRouter>
