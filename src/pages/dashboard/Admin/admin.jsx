390f8af45d0c31ff5e20a1e729436154bc232b74
Francisco Sanchez <f.sanchezsarverry@gmail.com>
2024-03-13 19:37:35 -0300

    1.0

---
+++ src/pages/dashboard/Admin/admin.jsx
@@ -0,0 +1,31 @@
+import { Container, Row, Col } from "react-bootstrap"
+import TablaProductos from "../../components/tablas/TablaProductos"
+import FormProductos from "../../components/formProductos/FormProductos"
+import TablaUsuario from "../../components/tablas/TablaUsuario"
+
+const Admin = () => {
+  return (
+    <>
+    <h2>Administrador</h2>
+    <Container>
+        <Row>
+            <Col>
+            <FormProductos />
+            </Col>
+            <Col>
+            <TablaProductos />
+            </Col>
+        </Row>
+        <Row>
+          <Col>
+          <TablaUsuario />
+          </Col>
+        </Row>
+    </Container>
+
+
+
+</>
+
+  )
+}
